716a31126a4ab51dd92e4ebe04eab9df23aaaf9f
Pedro Arturo de Leon Parra <deleonarturo09@gmail.com>
2023-11-14 01:42:06 -0400

    crear proyecto en angular 17 crear componente de header crear componente de footer crear componente login

---
+++ frontpruebasb/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontpruebasb';
+}
---
+++ frontpruebasb/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
